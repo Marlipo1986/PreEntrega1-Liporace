02b58306539d99976d9b7d5d86319a8011cd6873
Martin Liporace <marlipo1986@gmail.com>
2023-07-19 19:11:46 -0300

    Commit de calculadora integradora ultima version. Incluidos los algoritmos ciclicos (for)

--- main.js
+++ main.js
@@ -1,58 +1,74 @@
-function add(a, b) {
+function suma(a, b) {
     return a + b;
   }
   
-  function subtract(a, b) {
+  function resta(a, b) {
     return a - b;
   }
   
-  function multiply(a, b) {
+  function multiplica(a, b) {
     return a * b;
   }
   
   function divide(a, b) {
     if (b === 0) {
-      return "Error: Cannot divide by zero.";
+      return "Error: No es posible dividir por 0!";
     }
     return a / b;
   }
   
+  function eleva(a, b) { //Eleva A a la potencia de B
+    return a ** b;
+  }
+  
   function calculator() {
-    console.log("Calculator Operations:");
-    console.log("1. Addition");
-    console.log("2. Subtraction");
-    console.log("3. Multiplication");
-    console.log("4. Division");
+    console.log("Calculadora Básica:");
+    console.log("1. Suma");
+    console.log("2. Resta");
+    console.log("3. Multiplicación");
+    console.log("4. División");
+    console.log("5. Potencia");
+    console.log("6. Salir de la calculadora.")
   
-    const operation = 3 //Enter the operation number (1 to 4)
-  
-    if (operation < 1 || operation > 4) {
-      console.log("Invalid operation number. Please try again.");
+    while(true) {
+    
+    const operation = 6 //Ingrese el numero de operacion (1 a 6)
+        
+    if (operation === 6) {
+        console.log("Saliendo de la calculadora...Gracias!");
+        break;
+    }
+
+    if (operation < 1 || operation > 6) {
+      console.log("Operación desconocida. Vuelva a intentarlo");
       return;
     }
   
-    const num1 = 40;//Enter the first number
-    const num2 = 3;//Enter the second number
+    const num1 = 5;//Primer numero
+    const num2 = 3;//Segundo numero
   
     let result;
   
     switch (operation) {
-      case 1:
-        result = add(num1, num2);
-        break;
-      case 2:
-        result = subtract(num1, num2);
-        break;
-      case 3:
-        result = multiply(num1, num2);
-        break;
-      case 4:
-        result = divide(num1, num2);
-        break;
+        case 1:
+            result = add(num1, num2);
+            break;
+        case 2:
+            result = subtract(num1, num2);
+            break;
+        case 3:
+            result = multiply(num1, num2);
+            break;
+        case 4:
+            result = divide(num1, num2);
+            break;
+        case 5:
+            result = eleva(num1,num2);
+            break;   
     }
   
     console.log(`Result: ${result}`);
   }
-  
+  }
   calculator();
   
