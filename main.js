e4898fa450bbd0a3bb518dba1d689fccaff60c38
Martin Liporace <marlipo1986@gmail.com>
2023-07-20 00:28:53 -0300

    Arreglado un loop que no cerraba por el uso del WHILE

--- main.js
+++ main.js
@@ -1,73 +1,79 @@
 function suma(a, b) {
-    return a + b;
-  }
-  
-  function resta(a, b) {
-    return a - b;
-  }
-  
-  function multiplica(a, b) {
-    return a * b;
-  }
-  
-  function divide(a, b) {
-    if (b === 0) {
-      return "Error: No es posible dividir por 0!";
-    }
-    return a / b;
-  }
-  
-  function eleva(a, b) { //Eleva A a la potencia de B
-    return a ** b;
-  }
-  
-  function calculator() {
-    console.log("Calculadora Básica:");
-    console.log("1. Suma");
-    console.log("2. Resta");
-    console.log("3. Multiplicación");
-    console.log("4. División");
-    console.log("5. Potencia");
-    console.log("6. Salir de la calculadora. Gracias!")
-  
-    while(true) {
-    
-    const operation = 6 //Ingrese el numero de operacion (1 a 6)
-        
-    if (operation === 6) {
-        console.log("Saliendo de la calculadora...Gracias!");
-        break;
+  return a + b;
+}
 
-    if (operation < 1 || operation > 6) {
-      console.log("Operación desconocida. Vuelva a intentarlo");
-      return;
-    }
-  
-    const num1 = 5;//Primer numero
-    const num2 = 3;//Segundo numero
-  
-    let result;
-  
-    switch (operation) {
-        case 1:
-            result = add(num1, num2);
-            break;
-        case 2:
-            result = subtract(num1, num2);
-            break;
-        case 3:
-            result = multiply(num1, num2);
-            break;
-        case 4:
-            result = divide(num1, num2);
-            break;
-        case 5:
-            result = eleva(num1,num2);
-            break;   
-    }
-  
-    console.log(`Result: ${result}`);
+function resta(a, b) {
+  return a - b;
+}
+
+function multiplica(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: No es posible dividir por 0!";
   }
+  return a / b;
+}
+
+function eleva(a, b) { //Eleva A a la potencia de B
+  return a ** b;
+}
+
+function calculadora() {
+  console.log("Calculadora Básica:");
+  console.log("1. Suma");
+  console.log("2. Resta");
+  console.log("3. Multiplicación");
+  console.log("4. División");
+  console.log("5. Potencia");
+  console.log("6. Salir de la calculadora.")
   
-  calculator();
+  while(true) {
   
+  const operacion = 1 //Ingrese el numero de operacion (1 a 6)
+      
+  if (operacion === 6) {
+      console.log("Saliendo de la calculadora...Gracias!");
+      break;
+  }
+
+  if (operacion < 1 || operacion > 6) {
+    console.log("Operación desconocida. Vuelva a intentarlo");
+    return;
+  }
+
+
+  const num1 = 5;//Primer numero
+  const num2 = 2;//Segundo numero
+
+  let resultado;
+
+  switch (operacion) {
+      case 1:
+          resultado = suma(num1, num2);
+          break;
+      case 2:
+          resultado = resta(num1, num2);
+          break;
+      case 3:
+          resultado = multiplica(num1, num2);
+          break;
+      case 4:
+          resultado = divide(num1, num2);
+          break;
+      case 5:
+          resultado = eleva(num1,num2);
+          break;
+      default:
+          resultado = "Algo salió mal";
+          break;
+  }
+
+  console.log(`Result: ${resultado}`);
+  break
+}
+
+}
+calculadora();
